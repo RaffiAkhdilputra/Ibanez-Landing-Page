9ee4522fd61425cf4ae5989a7531669032ebdf39
Akhdilputra_ <49122160+RaffiAkhdilputra@users.noreply.github.com>
2025-03-26 02:24:19 +0700

    v3

--- fontawesome.js
+++
@@ -1,8 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-
-const all = [fas, fab, far]
-
-library.add(...all)
